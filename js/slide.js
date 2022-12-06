ab7906e5992ad69fec365ab3f3ce7de21e130788
suyeon95 <119912220+suyeon95@users.noreply.github.com>
2022-12-06 12:59:12 +0900

    Add files via upload

---
+++ js/slide.js
@@ -0,0 +1,9 @@
+ $(function(){
+ var swiper = new Swiper('.swiper-container', {
+	  autoplay: {
+        delay: 2500,//슬라이드 넘어가는 시간
+        disableOnInteraction: false,
+      },
+      loop: true,//슬라이드 무한반복
+    });
+});
